d3ad6800eac641095e649c0a64131656ff7e2381
Jeremiah Bargar <jeremy@formigaconsulting.com>
2023-11-07 11:59:32 -0500

    CONFIG: styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import "./App.css";
+import styled from "styled-components";
 
 function App() {
   return (
     <>
-      <div className="logo">🧮</div>
+      <Logo>🧮</Logo>
       <h1>take-home-table</h1>
       <div className="card">TODO</div>
     </>
   );
 }
+
+const Logo = styled.div`
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+  font-size: 96px;
+  &:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+`;
 
 export default App;

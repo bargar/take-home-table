8916c0da411a2b5736f2528cfba7d6190bd3f4df
Jeremiah Bargar <jeremy@formigaconsulting.com>
2023-11-08 12:57:20 -0500

    DOC: document sorting

--- src/components/take-home-table/sorting.ts
+++ src/components/take-home-table/sorting.ts
@@ -1,5 +1,15 @@
-import { SetSort, TakeHomeDataState } from "../../contexts/TakeHomeData.tsx";
+import { SetSort, TakeHomeDataState } from "./TakeHomeDataContext.tsx";
 
+/**
+ * Set appropriate next sorting:
+ *  * if unsorted by field, sort by field ascending
+ *  * if already sorted by field ascending, sort by same field descending
+ *  * if already sorted by field descending, clear sort and revert to natural ordering.
+ *
+ * @param fieldName
+ * @param state
+ * @param setSort
+ */
 export const setNextSort = (
   fieldName: string,
   state: TakeHomeDataState,
@@ -19,6 +29,13 @@ export const setNextSort = (
   }
 };
 
+/**
+ * Prose description of what next sort would be when clicking on a field's column, for benefit of the user.
+ * @param fieldName
+ * @param label
+ * @param title
+ * @param state
+ */
 export const nextSortForField = (
   fieldName: string,
   label: string,
